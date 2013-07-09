c4726ce0014cdbf6a65f25d7f4854490474db107
Gustavo Beauregard Noriega <gustavo.bn@feathermexico.com>
2013-07-08 23:41:50 -0400

    Queued mails cron
    Queued mails cron

--- lib/cron/send_mails.sh
+++ lib/cron/send_mails.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # * * * * * /home/itesm-student/projects/lastmile/lib/cron/send_mails.sh
 source /home/itesm-student/.bashrc
-cd /home/itesm-student/projects/lastmile/
+cd /home/itesm-student/projects/lastmile
 rake queued_mail:deliver
